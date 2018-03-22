b256150134f16609b909d081b5d295f2d43f6015
Danny Akimchuk <dakimchuk@ithaca.edu>
2018-03-22 11:31:16 -0400

    setup db

---
+++ db_scripts/V0__init.sql
@@ -0,0 +1,18 @@
+create table user (
+	id			bigint auto_increment not null,
+	name			varchar(255),
+	username		varchar(255),
+	password		varchar(255),
+	type			enum('client', 'employee', 'pharmacist'),
+	constraint pk_user primary key (id)
+);
+
+create table prescription (
+	id			bigint auto_increment not null,
+	name			varchar(255),
+	client_id		bigint,
+	is_validated		tinyint(1) default 0,
+	price			double,
+	constraint pk_order primary key(id)
+);
+
