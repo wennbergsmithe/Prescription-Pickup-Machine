d2f7ec6ac875e33a564ffd34f7877ab8c0090bab
Martin <Martin@Martins-MacBook-Pro.local>
2018-03-26 09:13:26 -0400

    Added Comments

--- src/main/java/edu.ithaca.group5/Employee.java
+++ src/main/java/edu.ithaca.group5/Employee.java
@@ -2,6 +2,10 @@ package edu.ithaca.group5;
 
 public class Employee extends User {
 
+    /**
+     * Takes in an Order object and calls the vadlidated setter to make it true.
+     * @param theOrder
+     */
     public void validateOrder(Order theOrder){
         theOrder.setValidated(true);
     }
--- src/main/java/edu.ithaca.group5/Pharmacist.java
+++ src/main/java/edu.ithaca.group5/Pharmacist.java
@@ -2,6 +2,15 @@ package edu.ithaca.group5;
 
 public class Pharmacist extends Employee {
 
+    /**
+     * Takes in an id, name, client object and price.
+     * Creates a new Order object with the Order constructor.
+     * @param inId
+     * @param inName
+     * @param inClient
+     * @param inPrice
+     * @return theOrder
+     */
     public Order createOrder(long inId, String inName, Client inClient, double inPrice){
         Order theOrder = new Order( inId, inName, inClient, inPrice);
         return theOrder;
