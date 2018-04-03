d6270498ddac72874f611d5600a1638e29ef7c7c
Martin <Martin@Martins-MacBook-Pro.local>
2018-04-03 16:56:19 -0400

    Payment method/tests

--- src/main/java/edu.ithaca.group5/User.java
+++ src/main/java/edu.ithaca.group5/User.java
@@ -5,11 +5,13 @@ public class User {
     String name;
     String username;
     String password; //TODO: make secure
+    double balance;
 
     protected User(long id, String name, String username, String password) {
         this.id = id;
         this.name = name;
         this.username = username;
         this.password = password;
+        this.balance = balance;
     }
 }
