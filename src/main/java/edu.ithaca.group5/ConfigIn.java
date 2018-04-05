07ec4e8de4b169a2fbc16f6b2838b1b94152c1cc
Danny Akimchuk <dakimchuk@ithaca.edu>
2018-04-05 10:27:04 -0400

    circleci setup

--- src/main/java/edu.ithaca.group5/ConfigIn.java
+++ src/main/java/edu.ithaca.group5/ConfigIn.java
@@ -2,7 +2,7 @@ package edu.ithaca.group5;
 
 public class ConfigIn {
     public static final String DB_HOST = "jdbc:mysql://localhost:3306/INSERT_DB_NAME_HERE";
-    public static final String TEST_DB_HOST = "jdbc:mysql://localhost:3306/INSERT_TEST_DB_NAME_HERE";
-    public static final String DB_USER = "SQL_USERNAME";
-    public static final String DB_PASSWORD = "SQL_PASSWORD";
+    public static final String TEST_DB_HOST = "jdbc:mysql://localhost:3306/test_dd";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "";
 }
