eb5808ae95877c7b4aa78110f5bbfdb1d6f9e0ba
groth12 <groth1@ithaca.edu>
2018-04-05 20:24:27 -0400

    first edits of v2, has an error

--- src/main/java/edu.ithaca.group5/ConfigIn.java
+++
@@ -1,8 +0,0 @@
-package edu.ithaca.group5;
-
-public class ConfigIn {
-    public static final String DB_HOST = "jdbc:mysql://localhost:3306/INSERT_DB_NAME_HERE";
-    public static final String TEST_DB_HOST = "jdbc:mysql://localhost:3306/INSERT_TEST_DB_NAME_HERE";
-    public static final String DB_USER = "SQL_USERNAME";
-    public static final String DB_PASSWORD = "SQL_PASSWORD";
-}
