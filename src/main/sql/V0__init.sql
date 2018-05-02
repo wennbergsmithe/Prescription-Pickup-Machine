50ab4a043d2cb242e7bd77172dd8167281796de5
wennbergsmithe <wennbergsmithe@gmail.com>
2018-05-01 22:08:38 -0400

    conficts resolved?

--- src/main/sql/V0__init.sql
+++ src/main/sql/V0__init.sql
@@ -6,7 +6,7 @@ create table user (
 	balance         double,
 	type			enum('client', 'employee', 'pharmacist'),
 	isFrozen	BOOL DEFAULT FALSE,
-	salt			varchagur(255),
+	salt			varchar(255),
 	allergies       varchar(255),
 	constraint pk_user primary key (id)
 );
